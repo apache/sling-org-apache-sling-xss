2ece13650f5edbde738ab8fe9ebbaf47b0435101
Radu Cotescu <radu@apache.org>
2019-01-25 13:03:57 +0100

    SLING-8235 - Stop copying the AntiSamy configuration to the repository
    * removed unused imports

--- src/test/java/org/apache/sling/xss/impl/XSSFilterImplTest.java
+++ src/test/java/org/apache/sling/xss/impl/XSSFilterImplTest.java
@@ -18,17 +18,16 @@
  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 package org.apache.sling.xss.impl;
 
-import org.apache.sling.api.resource.PersistenceException;
-import org.apache.sling.api.resource.Resource;
 import org.apache.sling.serviceusermapping.ServiceUserMapped;
 import org.apache.sling.testing.mock.sling.junit.SlingContext;
 import org.apache.sling.xss.XSSFilter;
 import org.junit.After;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 
 public class XSSFilterImplTest {
